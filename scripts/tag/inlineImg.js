524b4d12e30ab1bc6341ecc5290d9cc48a8066c4
dawnkylin <dawnkylin@163.com>
2025-08-21 21:52:46 +0800

    fix: 解决 gallery 图片实际宽高比与显示宽高比不同 fix: 为 inlineImg 增加空 alt 属性 fix: 为普通 img 设置 arial-hidden 属性为 true 避免重复说明性文本

--- scripts/tag/inlineImg.js
+++ scripts/tag/inlineImg.js
@@ -13,7 +13,7 @@ const urlFor = require('hexo-util').url_for.bind(hexo)
 const inlineImg = ([img, height = '']) => {
   const heightStyle = height ? `style="height:${height}"` : ''
   const src = urlFor(img)
-  return `<img class="inline-img" src="${src}" ${heightStyle} />`
+  return `<img class="inline-img" src="${src}" ${heightStyle} alt="" />`
 }
 
 hexo.extend.tag.register('inlineImg', inlineImg, { ends: false })
